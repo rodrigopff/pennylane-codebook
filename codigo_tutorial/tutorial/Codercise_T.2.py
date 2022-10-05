2155e04e51a599019e5e96332c07269203eb6524
rodrigopff <rodrigopff@gmail.com>
2022-10-05 10:23:04 -0300

    Primeiros exercicios da introducao

---
+++ codigo_tutorial/tutorial/Codercise_T.2.py
@@ -0,0 +1,17 @@
+ 
+
+def find_max(x):
+    """A simple function to find the largest value in a numpy array.
+
+    Args:
+        x (array[float]): an array of numbers of size n
+
+    Returns:
+        float: The maximum value in x.
+    """
+
+    ##################
+    # YOUR CODE HERE #
+    ##################
+
+    return np.max(x)
