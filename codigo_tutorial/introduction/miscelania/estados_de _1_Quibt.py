e8f51d755f3c942135a6cb189f6a8050ead6eef7
rodrigopff <rodrigopff@gmail.com>
2023-07-21 17:51:27 -0300

    Inclusao de novas funcoes

--- codigo_tutorial/introduction/miscelania/estados_de _1_Quibt.py
+++ codigo_tutorial/introduction/miscelania/estados_de _1_Quibt.py
@@ -1,8 +1,10 @@
-from locale import ABMON_10
+
 import numpy as np
 from sympy import *
 import pennylane as qml
 import sys
+from sympy.physics.quantum import Bra,Ket
+
 
 # Ajeitar pra pegar a entrada  
 
@@ -24,6 +26,20 @@ ketsBrasMap = {
     "<+|": "|+>",
     "<-|": "|->"
 }
+
+## Representa um operador geral na base {|+>,|->}, em notacao de Dirac
+## Será utilizado para se escrever em tela o operador correspondente depois de calculados os seus Aijs
+#listaProdutosExterioresNaBaseMaisMenos = [ "|+><+|", "|+><-|", "|-><+|", "|-><-|" ]
+
+labelsNaBaseMaisMenos = [ ["+","+"], ["+","-"], ["-","+"], ["-","-"] ]
+ 
+
+#mapaProdutosExterioresNaBaseMaisMenos = {
+#    0: "|+><+|",
+#    1: "|+><-|",
+#    2: "|-><+|",
+#    3: "|-><-|"
+#}
 
 ############################################################
 ##  Exemplo de circuito para preparar um estado de 1 qbit ##
@@ -50,9 +66,9 @@ def preparaEstado(valor) :
         vetorEstado = Matrix(preparaEstado_circuit(valor))                
     
     ## Prepara um bra (transposto conjugado)
-    elif valor in bras : # a funcao de conjugado apliacada abaixo nao funciona corretamete em tensores vindos do cirtuico do pennylanne
+    elif valor in bras : # a funcao de conjugado aplicada abaixo nao funciona corretamete em tensores vindos do circuico do pennylanne
                          # portanto foi necessario aplica-la ao tipo Matrix e reaplicar o resultato do np.darray 
-                         # ao uma nova Matrix
+                         # a uma nova Matrix
         vetorEstado = Matrix(np.conjugate(Matrix(preparaEstado_circuit(ketsBrasMap[valor])))).T              
 
     return vetorEstado    
@@ -64,8 +80,6 @@ def calculaMatrizDoOperadorNaBaseComputacional(operador):
     operadorSemEspacos = operador.replace(" ","")
     ## continuar aqui e verifica se a string eh maior que 6 
     print(len(operadorSemEspacos))                                                                                                                                                                                                                                                                                                      
-
-
 
 #print(preparaEstado("<-|"))
 #pprint(preparaEstado("<-|"))
@@ -86,12 +100,63 @@ ket_mais = preparaEstado("|+>")
 bra_mais = preparaEstado("<+|")
 ket_menos = preparaEstado("|->")
 bra_menos = preparaEstado("<-|")
- 
-# Multiplica-se e soma-se os prdutos exteriores da expressao  Y=−i|0><1|+i|1><0|
 
+## Cacula os elementos Aij do operador na base de {|+),|->}
+def calculaAijs (A) :
+    Aij = []
+    A_mais_mais   = (bra_mais  * A * ket_mais)[0]  # <+| A |+>
+    A_mais_menos  = (bra_mais  * A * ket_menos)[0] # <+| A |-> 
+    A_menos_mais  = (bra_menos * A * ket_mais)[0]  # <-| A |+>
+    A_menos_menos = (bra_menos * A * ket_menos)[0] # <-| A |->  
+    Aij.append(A_mais_mais)
+    Aij.append(A_mais_menos)
+    Aij.append(A_menos_mais)
+    Aij.append(A_menos_menos)    
+    return Aij
+
+## Cria em notacao de Dirac o operador, dado os seus Aijs 
+
+def operadorEmNotacaoDeDirac (A,base) :
+    #base = listaProdutosExterioresNaBaseMaisMenos
+    base = labelsNaBaseMaisMenos
+    #operadorEmNotacaoDeDirac = []
+    operadorEmNotacaoDeDirac = 0
+    for i in range(len(A)):
+        if A[i] != 0 :
+            operadorEmNotacaoDeDirac = operadorEmNotacaoDeDirac + A[i]*Ket(base[i][0])*Bra(base[i][1])
+    return operadorEmNotacaoDeDirac
+
+# PORTA X na base {|0>,|1>}
+# Multiplica-se e soma-se os produtos exteriores da expressao  X = |0><1|+|1><0
+X = 1*(ket_0 * bra_1) + 1*(ket_1 * bra_0)
+print()
+print("#################################")
+print("## MATRIZ X NA BASE {|0>,|1>} ###")
+print("#################################")
+pprint(X)
+print()
+# PORTA y na base {|0>,|1>}
+# Multiplica-se e soma-se os produtos exteriores da expressao  Y = −i|0><1|+i|1><0|
 Y = -1j*(ket_0 * bra_1) + 1j*(ket_1 * bra_0)
-
+print("#################################")
+print("## MATRIZ Y NA BASE {|0>,|1>} ###")
+print("#################################")
 pprint(Y)
+print("#################################")
+print()
+# PORTA Z na base {|0>,|1>}
+# Multiplica-se e soma-se os produtos exteriores da expressao  Z = |0><0|−|1><1|
+Z = 1*(ket_0 * bra_0) - 1*(ket_1 * bra_1)
+print("#################################")
+print("## MATRIZ Z NA BASE {|0>,|1>} ###")
+print("#################################")
+#print()
+pprint(Z) 
+#print()
+print("#################################")
+print()
+#pprint(Z)
+
 
 #A00 = bra_mais * Y * ket_mais
 #A01 = bra_mais * Y * ket_menos
@@ -99,29 +164,63 @@ pprint(Y)
 #A11 = bra_menos * Y * ket_menos
 
 ## como retorna um matriz 1x1 (um so elemento) pego o valor do mesmo com o indice [0] 
-Aij = []
-A_mais_mais = (bra_mais * Y * ket_mais)[0]
-A_mais_menos = (bra_mais * Y * ket_menos)[0]
-A_menos_mais = (bra_menos * Y * ket_mais)[0]
-A_menos_menos = (bra_menos * Y * ket_menos)[0]
-Aij.append(A_mais_mais)
-Aij.append(A_mais_menos)
-Aij.append(A_menos_mais)
-Aij.append(A_menos_menos)
+#Aij = []
+#A_mais_mais = (bra_mais * Y * ket_mais)[0]
+#A_mais_menos = (bra_mais * Y * ket_menos)[0]
+#A_menos_mais = (bra_menos * Y * ket_mais)[0]
+#A_menos_menos = (bra_menos * Y * ket_menos)[0]
+#Aij.append(A_mais_mais)
+#Aij.append(A_mais_menos)
+#Aij.append(A_menos_mais)
+#Aij.append(A_menos_menos)  
 
-estadoNaBaseMaisMenoshMap = {
-    Aij[0]: "|+><+|",
-    Aij[0]: "|+><-|",
-    Aij[0]: "|-><+|",
-    Aij[0]: "|-><-|"
-}
-  
+#pprint(A_mais_mais)
+#pprint(A_mais_menos)
+#pprint(A_menos_mais)
 
-pprint(A_mais_mais)
-pprint(A_mais_menos)
-pprint(A_menos_mais)
-pprint(A_menos_menos)
+#pprint(A_menos_menos)
+
+# Calcula-se os elementos da matriz de X na base {|+>,|->}
+Xijs = calculaAijs(X)
+#pprint(Xijs)
+print("#################################")
+print("## MATRIZ X NA BASE {|+>,|->} ###")
+print("#################################")
+pprint(Matrix([[Xijs[0],Xijs[1]], [Xijs[2],Xijs[3]]]))
+print("#################################")
+print()
+
+pprint(operadorEmNotacaoDeDirac(Xijs,""))
+
+# Calcula-se os elementos da matriz de Y na base {|+>,|->}
+Yijs = calculaAijs(Y)
+#pprint(Yijs)
+print("#################################")
+print("## MATRIZ Y NA BASE {|+>,|->} ###")
+print("#################################")
+pprint(Matrix([[Yijs[0],Yijs[1]], [Yijs[2],Yijs[3]]]))
+print("#################################")
+print()
+pprint(operadorEmNotacaoDeDirac(Yijs,""))
+
+# Calcula-se os elementos da matriz de Z na base {|+>,|->}
+Zijs = calculaAijs(Z)
+#pprint(Zijs)
+print("#################################")
+print("## MATRIZ Z NA BASE {|+>,|->} ###")
+print("#################################")
+pprint(Matrix([[Zijs[0],Zijs[1]], [Zijs[2],Zijs[3]]]))
+print("#################################")
+print()
+pprint(operadorEmNotacaoDeDirac(Zijs,""))
+
+
 #pprint(bra_mais * Y * ket_mais)
 #pprint(bra_mais * Y * ket_menos)
 #pprint(bra_menos * Y * ket_mais)
 #pprint(bra_menos * Y * ket_menos)
+
+#b = Bra('psi')
+#k = Ket('psi')
+
+#pprint((I*b) * (I*k))
